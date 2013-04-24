74d75ff04446f2f9049668853310360815da23eb
Emem Adegbola <doublement@gmail.com>
2013-04-24 23:12:31 +0100

    Added an entertainment value pair data structure

---
+++ TACAgent/se/sics/tac/datastructures/EntertainmentTypes.java
@@ -0,0 +1,5 @@
+package se.sics.tac.datastructures;
+
+public enum EntertainmentTypes 
+{aligatorWrestling, amusementPark, museum;
+}
---
+++ TACAgent/se/sics/tac/datastructures/EntertainmentValuePair.java
@@ -0,0 +1,23 @@
+package se.sics.tac.datastructures;
+
+//A datatype to store a customer and value in a pair. This class will sort by value
+public class EntertainmentValuePair implements Comparable<ClientValuePair>{
+	public EntertainmentTypes type;
+	public float value;
+	
+	public EntertainmentValuePair(EntertainmentTypes type, float value) {
+		this.type = type;
+		this.value = value;
+	}
+
+	@Override
+	public int compareTo(ClientValuePair o) {
+		if (this.value < o.value) {
+			return -1;
+		} else if (this.value > o.value) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+}
