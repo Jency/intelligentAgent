ed58aa17849d85bac6c49532ed0750d131d92f9f
Emem Adegbola <doublement@gmail.com>
2013-04-30 20:59:34 +0100

    Updated sub agents to use less processor power

--- TACAgent/se/sics/tac/agent/HotelAgent.java
+++ TACAgent/se/sics/tac/agent/HotelAgent.java
@@ -1,6 +1,16 @@
 package se.sics.tac.agent;
 
+
+import java.util.concurrent.Semaphore;
+
+import javax.swing.Timer;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.concurrent.Semaphore;
+
 public class HotelAgent extends SubAgent {
+	Semaphore timingSemaphore;
+	int FREQUENCY = 1; // How often to repeat the main loop in seconds
 
 	@Override
 	public void initialise() {
@@ -9,8 +19,27 @@ public class HotelAgent extends SubAgent {
 
 	@Override
 	public void run() {
-		while (true){
-			// TODO: Write agent logic
+		// Set up the delay timer 
+		int delay = 1000 * FREQUENCY;
+		timingSemaphore = new Semaphore(1);
+		ActionListener taskPerformer = new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				timingSemaphore.release();
+			}
+		};
+		new Timer(delay, taskPerformer).start();
+
+		while (true) {
+			try {
+				timingSemaphore.acquire();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			
+			// Program logic here
+			
+			
 		}
 	}
 }
